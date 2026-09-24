7d773b85a07f540a58420be9c0621e963097fd33
mean-stack-devhub-sagar <prasadsagar011@gmail.com>
2026-09-24 22:21:36 +0530

    Force IPv4 for Gmail SMTP

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -4,7 +4,7 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
-  requireTLS: true,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -14,9 +14,9 @@ const transporter = nodemailer.createTransport({
 const sendEmail = async (to, subject, text) => {
   const result = await transporter.sendMail({
     from: `"Sanika Makeover" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    text,
+    to: to,
+    subject: subject,
+    text: text,
   });
 
   return result;
