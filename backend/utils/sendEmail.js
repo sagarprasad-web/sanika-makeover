824370256e194e0550959f7dd62545498433c98b
mean-stack-devhub-sagar <prasadsagar011@gmail.com>
2026-09-24 18:21:58 +0530

    Fix Gmail SMTP connection

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,7 +1,10 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -11,9 +14,9 @@ const transporter = nodemailer.createTransport({
 const sendEmail = async (to, subject, text) => {
   const result = await transporter.sendMail({
     from: `"Sanika Makeover" <${process.env.EMAIL_USER}>`,
-    to: to,
-    subject: subject,
-    text: text,
+    to,
+    subject,
+    text,
   });
 
   return result;
